70faf1dc2f2b3bcd779dc7e57715d547fe91b80d
Mike Young <mikeyoung3k@gmail.com>
2025-12-04 05:49:37 +0000

    Day 4

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ const BASE_DIR: &'static str = "C://Users/Mike_/Documents/Rust/advent_of_code/ao
 
 mod day1;
 mod day2;
+mod day3;
+mod day4;
 
 fn main() {
     // let (r1,r2) = day1::run();
     // println!("Day 1 part 1: {}, Day 1 part 2: {}", r1, r2);
-    let (r1,r2) = day2::run();
-    println!("Day 2 part 1: {}, Day 2 part 2: {}", r1, r2);
+    // let (r1,r2) = day2::run();
+    // println!("Day 2 part 1: {}, Day 2 part 2: {}", r1, r2);
+    // let (r1,r2) = day3::run();
+    // println!("Day 3 part 1: {}, Day 3 part 2: {}", r1, r2);
+    let (r1,r2) = day4::run();
+    println!("Day 4 part 1: {}, Day 4 part 2: {}", r1, r2);
 }
